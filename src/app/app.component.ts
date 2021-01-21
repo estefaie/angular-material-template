7b7d422a2ea414b3f7eda9cfc172020c5945f106
Hemen Estefaie <estefaie@gmail.com>
2021-01-21 22:51:35 +1100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+/**
+ * @title Basic use of the tab group
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  title = 'one-stop';
-}
+export class App {}
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
